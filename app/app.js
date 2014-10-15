0dc80ac99d22a0ce53d1a942a774d15bffdb9711
bartosmarcin <bartosmarcin@gmail.com>
2014-10-15 19:11:46 +0200

    przykladowa lista zamowien

--- app/app.js
+++ app/app.js
@@ -1,12 +1,18 @@
 'use strict';
+(function () {
+    var app = angular.module('ordersManagement', []);
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    app.controller('OrdersListCtrl', [function () {
+        this.orders = orders;
+    }]);
+
+    var orders = [
+        {
+            id:1,
+            name:"trololo"
+        },
+        {
+            id:2,
+            name:"order2"
+        }];
+})();
